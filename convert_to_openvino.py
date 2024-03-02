170c71ce44adb7eb33e49662ae0c03948ab9e0e7
Ayush Kaura <ayushkaura@gmail.com>
2024-03-02 18:27:07 +0530

    Modify convert_to_openvino

--- convert_to_openvino.py
+++ convert_to_openvino.py
@@ -1,15 +1,19 @@
 import openvino as ov
+import onnx
 
-ov_model = ov.convert_model("/home/ayush/fyp/model.onnx")
-ov.save_model(ov_model, "model.xml", compress_to_fp16=True)
+onnx_model = onnx.load("model_gdnet.onnx")
+onnx.checker.check_model(onnx_model, True)
+
+ov_model = ov.convert_model("/home/ayush/OptiDepth/model_gdnet.onnx")
+ov.save_model(ov_model, "model_gdnet.xml", compress_to_fp16=True)
 print("Exported.")
 
-# import blobconverter
+import blobconverter
 
-# NN_PATH = blobconverter.from_openvino(
-#     "/home/ayush/fyp/model.xml", "/home/ayush/fyp/model.bin", shaves=6)
+NN_PATH = blobconverter.from_openvino(
+    "/home/ayush/OptiDepth/model_gdnet.xml", "/home/ayush/OptiDepth/model_gdnet.bin", shaves=6)
 
-# print(NN_PATH)
+print(NN_PATH)
     
 # "stderr": "Cannot create Interpolate layer /h5_up/Resize id:969 from unsupported opset: opset11\n",
 # "stdout": "OpenVINO Runtime version ......... 2022.1.0\nBuild ........... 2022.1.0-7019-cdb9bec7210-releases/2022/1\n"
